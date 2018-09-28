ef87d2e8f7532a0f68a5e7669a1d0567737daaab
wangzhichao <392337950@qq.com>
2018-09-29 01:28:07 +0800

    p1_simple_factory init

---
+++ p1_simple_factory/src/main/java/com/example/p1_simple_factory/MainActivity.java
@@ -0,0 +1,12 @@
+package com.example.p1_simple_factory;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
